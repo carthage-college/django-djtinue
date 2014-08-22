4d8e12cbd97ca4ca244778b6d795723c5044fbc1
Bender <plungerman@gmail.com>
2014-08-22 15:24:40 -0500

    updated shell script for snatching events

--- djtinue/bin/info_session_events.py
+++ djtinue/bin/info_session_events.py
@@ -1,48 +1,46 @@
 from django.db import connections
-from djtinue.admissions.models import LivewhaleEvents
-from djtinue.admissions.forms import STYPES
-
 from django.utils.timezone import localtime
 from django.utils.dateformat import DateFormat
 
-from django.utils.encoding import smart_text, force_text
+from djtinue.admissions.models import LivewhaleEvents
 
+# dictionary name corresponds to URL slug
+STYPES = {
+    "information-session":712,
+    "graduate-education":715,
+    "undergraduate-studies":713,
+    "master-social-work":714,
+    "paralegal":582
+}
+
+session_type="paralegal"
 cursor = connections['livewhale'].cursor()
-
-session_type="undergraduate-studies"
-
 sql = """
-    SELECT
-        id,title,date_dt
-    FROM
-        livewhale_events
-    WHERE
-        id IN (
-            select id2 from livewhale_tags2any where id1=%s
-        )
-    AND
-        id IN (
-            select id2 from livewhale_tags2any where id1=%s
-        )
-    AND
-        date_dt > DATE(NOW())
-    ORDER BY
-        date_dt
+            SELECT
+                id,title,date_dt
+            FROM
+                livewhale_events
+            WHERE
+                id IN (
+                    select id2 from livewhale_tags2any where id1=%s
+                )
+            AND
+                id IN (
+                    select id2 from livewhale_tags2any where id1=%s
+                )
+            AND
+                date_dt > DATE(NOW())
+            ORDER BY
+                date_dt
 """ % (STYPES["information-session"],STYPES[session_type])
-
-print sql
-
 cursor.execute(sql)
 
-# format:
-# Thu. May 01, 2014 at 06pm (Master of Education & ACT Info Session)
 for event in cursor.fetchall():
     lc = localtime(event[2])
     df = DateFormat(lc)
     day = df.format('D')
     date = df.format('M d, Y')
     time = df.format('h:ia')
-    event = force_text(event[1])
-    title = "%s. %s at %s (%s)" % (day, date, time , event)
-
+    title = "%s. %s at %s (%s)" % (day, date, time , force_text(event[1]))
     print title
+
