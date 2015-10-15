ac79a65952ae1344922729519c443b42dd599477
Bender <plungerman@gmail.com>
2015-10-15 14:53:58 -0500

    forgot to add the enrichment courses registration admin

---
+++ djtinue/enrichment/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from djtinue.enrichment.models import Course, Registration
+
+class CourseAdmin(admin.ModelAdmin):
+    model = Course
+
+
+class CourseInline(admin.TabularInline):
+    model = Registration.courses.through
+    #fields = ('title', 'abstract',)
+
+
+class RegistrationAdmin(admin.ModelAdmin):
+    model = Registration
+    list_display = (
+        'first_name', 'last_name', 'email','created_at'
+    )
+    search_fields = ('last_name', 'email',)
+    ordering = ['-created_at',]
+    inlines = (CourseInline,)
+    raw_id_fields = ("order",)
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Registration, RegistrationAdmin)
