82b753c9381f8bab29fadff829f1ab61f8fd74b3
Bender <plungerman@gmail.com>
2016-01-19 10:09:57 -0600

    updated TO_LIST with GPS emails and admin manager

--- djtinue/enrichment/__init__.py
+++ djtinue/enrichment/__init__.py
@@ -1,9 +1,14 @@
 from django.conf import settings
 
 if settings.DEBUG:
-    TO_LIST = [settings.SERVER_EMAIL,]
+    #TO_LIST = [settings.SERVER_EMAIL,]
+    TO_LIST = [
+        settings.CONTINUING_STUDIES_ENRICHMENT_REGISTRATION_EMAIL,
+        settings.ADMINS[0][1]
+    ]
 else:
     TO_LIST = [
         settings.CONTINUING_STUDIES_ENRICHMENT_REGISTRATION_EMAIL,
+        settings.ADMINS[0][1]
     ]
 BCC = settings.MANAGERS
