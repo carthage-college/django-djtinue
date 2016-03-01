e98c4b6d74069fc6a7b0d7ac71ea020646378059
Bender <plungerman@gmail.com>
2016-03-01 09:14:21 -0600

    changed group name from 'graduate and professional studies' to 'continuing studies'; minor tweaks to wsgi default configuration

--- djtinue/wsgi_default.py
+++ djtinue/wsgi_default.py
@@ -1,26 +1,46 @@
-import os, sys
+"""
+WSGI config for djtinue project.
 
-# python
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
+"""
+
+import os
+import time
+import traceback
+import signal
+import sys
+
 sys.path.append('/usr/lib/python2.7/')
-#sys.path.append('/data2/django_current/')
-#sys.path.append('/data2/django_1.6/')
-sys.path.append('/data2/django_1.7/')
+sys.path.append('/data2/django_1.8/')
 sys.path.append('/data2/django_projects/')
 sys.path.append('/data2/django_third/')
-# django
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djtinue.settings")
 os.environ.setdefault("PYTHON_EGG_CACHE", "/var/cache/python/.python-eggs")
 os.environ.setdefault("TZ", "America/Chicago")
+
 # informix
 os.environ['INFORMIXSERVER'] = ''
 os.environ['DBSERVERNAME'] = ''
-os.environ['INFORMIXDIR'] = '/opt/ibm/informix'
-os.environ['ODBCINI'] = '/etc/odbc.ini'
+os.environ['INFORMIXDIR'] = ''
+os.environ['ODBCINI'] = ''
 os.environ['ONCONFIG'] = ''
-os.environ['INFORMIXSQLHOSTS'] = '/opt/ibm/informix/etc/sqlhosts'
-os.environ['LD_LIBRARY_PATH'] = '$INFORMIXDIR/lib:$INFORMIXDIR/lib/esql:$INFORMIXDIR/lib/tools:/usr/lib/apache2/modules:$INFORMIXDIR/lib/cli'
-os.environ['LD_RUN_PATH'] = '/opt/ibm/informix/lib:/opt/ibm/informix/lib/esql:/opt/ibm/informix/lib/tools:/usr/lib/apache2/modules'
-# wsgi
+os.environ['INFORMIXSQLHOSTS'] = ''
+
+os.environ['LD_LIBRARY_PATH'] = ''
+os.environ['LD_RUN_PATH'] = ''
+
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+
+try:
+    application = get_wsgi_application()
+except Exception:
+    # Error loading applications
+    if 'mod_wsgi' in sys.modules:
+        traceback.print_exc()
+        os.kill(os.getpid(), signal.SIGINT)
+        time.sleep(2.5)
 
