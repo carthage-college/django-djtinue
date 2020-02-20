f634b295c65b4b264912814c564208c6f2336085
plungerman <plungerman@gmail.com>
2020-02-20 12:30:50 -0600

    added django accounts URLs

--- djtinue/urls.py
+++ djtinue/urls.py
@@ -1,15 +1,32 @@
 from django.conf.urls import include, url
 from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_views
 
 from django.contrib import admin
 
-#admin.autodiscover()
+from djauth.views import loggedout
 
 
 urlpatterns = [
     url(
         r'^admin/', include(admin.site.urls)
     ),
+    # auth
+    url(
+        r'^accounts/login/$', auth_views.login,
+        {'template_name': 'accounts/login.html'},
+        name='auth_login'
+    ),
+    url(
+        r'^accounts/logout/$', auth_views.logout,
+        {'next_page': '/continuing-studies/forms/accounts/loggedout/'},
+        name='auth_logout'
+    ),
+    url(
+        r'^accounts/loggedout', loggedout,
+        {'template_name': 'accounts/logged_out.html'}
+    ),
+    # apps
     url(
         r'^admissions/', include('djtinue.admissions.urls')
     ),
